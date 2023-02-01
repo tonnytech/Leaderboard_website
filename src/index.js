6e9c6e409b7a29fcd6b019d502e6db566307cdf5
teitonny <tonnytei4@gmail.com>
2023-02-01 12:36:14 +0300

    added linter setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import './index.css';
 
-import data_array from "./modules/data";
+import dataArray from './modules/data.js';
 
 const table = document.querySelector('.table');
 
-data_array.forEach( (data) => {
-const row = document.createElement('tr');
-const name = document.createElement('td');
-name.innerHTML = data.name;
-const score = document.createElement('td');
-score.innerHTML = data.score;
-row.appendChild(name);
-row.appendChild(score);
-table.appendChild(row);
-return table;
+dataArray.forEach((data) => {
+  const row = document.createElement('tr');
+  const name = document.createElement('td');
+  name.innerHTML = data.name;
+  const score = document.createElement('td');
+  score.innerHTML = data.score;
+  row.appendChild(name);
+  row.appendChild(score);
+  table.appendChild(row);
+  return table;
 });
-
--- src/modules/data.js
+++ src/modules/data.js
@@ -1,24 +1,24 @@
-const data_array = [
-    {
-        name: 'name:',
-        score: '100'
-    },
-    {
-        name: 'name:',
-        score: '20'
-    },
-    {
-        name: 'name:',
-        score: '10'
-    },
-    {
-        name: 'name:',
-        score: '70'
-    },
-    {
-        name: 'name:',
-        score: '80'
-    }
+const dataArray = [
+  {
+    name: 'name:',
+    score: '100',
+  },
+  {
+    name: 'name:',
+    score: '20',
+  },
+  {
+    name: 'name:',
+    score: '10',
+  },
+  {
+    name: 'name:',
+    score: '70',
+  },
+  {
+    name: 'name:',
+    score: '80',
+  },
 ];
 
-export default data_array;
+export default dataArray;
